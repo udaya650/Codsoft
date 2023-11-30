e181c0dcdf5879bf4795475e349a428765b0a85a
udaya650 <n190650@rguktn.ac.in>
2023-11-30 22:58:35 +0530

    no changes

---
+++ landing-page.js
@@ -0,0 +1,16 @@
+let firstPartButton=document.getElementById("firstPartButton");
+firstPartButton.addEventListener("mouseenter",function(){
+    firstPartButton.style.width="200px";
+});
+firstPartButton.addEventListener("mouseleave",function(){
+    firstPartButton.style.width="170px";
+});
+
+
+let firstPartButton1=document.getElementById("firstPartButton1");
+firstPartButton1.addEventListener("mouseenter",function(){
+    firstPartButton1.style.width="200px";
+});
+firstPartButton1.addEventListener("mouseleave",function(){
+    firstPartButton1.style.width="170px";
+});
